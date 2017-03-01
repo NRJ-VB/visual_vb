4bb9115a599ef2a8c55cc5d8c915baf7fa4ddee4
Neeraj55#5 <thakurfazer55@gmail.com>
2017-03-01 15:22:11 +0530

    Add Voter Tester Form

--- WindowsApplication1/My Project/Application.Designer.vb
+++ WindowsApplication1/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApplication1.EventHandler
+            Me.MainForm = Global.WindowsApplication1.voter
         End Sub
     End Class
 End Namespace
---
+++ WindowsApplication1/voter.vb
@@ -0,0 +1,13 @@
+﻿Public Class voter
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim age As Integer
+        age = TextBox2.Text
+        If (age >= 18) Then
+            MsgBox(TextBox1.Text & " you can vote")
+        Else
+            MsgBox("Sorry " & TextBox1.Text & " YOU are not eligble for voting")
+
+
+        End If
+    End Sub
+End Class
