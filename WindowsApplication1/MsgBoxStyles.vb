92379f6ddba18cebe259863077723c115d23ef8c
Neeraj55#5 <thakurfazer55@gmail.com>
2017-03-03 15:33:01 +0530

    Initial Validation Example

---
+++ WindowsApplication1/MsgBoxStyles.vb
@@ -0,0 +1,23 @@
+﻿Public Class MsgBoxStyles
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim msg As MsgBoxResult
+        msg = MsgBox("Are u old as 21 to join", MsgBoxStyle.YesNo)
+        If msg = MsgBoxResult.Yes Then
+            MsgBox("You can join")
+        ElseIf msg = MsgBoxResult.No Then
+            MsgBox("You can't join")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim a As MsgBoxResult
+        a = MsgBox("Are YOU sure", MsgBoxStyle.AbortRetryIgnore)
+        If a = MsgBoxResult.Yes Then
+            MsgBox("OK")
+        ElseIf a = MsgBoxResult.no Then
+            MsgBox("Try again")
+        ElseIf a = msgboxresult.cancel Then
+            MsgBox("LOL")
+        End If
+    End Sub
+End Class
---
+++ WindowsApplication1/Validation.Designer.vb
@@ -0,0 +1,117 @@
+﻿Imports System.Text.RegularExpressions
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Validation
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(314, 68)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(179, 27)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(314, 127)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(179, 29)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Slant", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(206, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 36)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Slant", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(196, 117)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(112, 39)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Phone"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Slant", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(329, 176)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(153, 62)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Check"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Validation
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(933, 334)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Validation"
+        Me.Text = "Validation"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As MsgBoxResult
+        Dim phone As New Regex("\d{3}-\d{10}")
+        If phone.IsMatch(TextBox2.Text) Then
+            a = MsgBox("Your Name is " & TextBox1.Text & " " & " and phone :" & TextBox2.Text & "?", MsgBoxStyle.YesNo)
+            If a = MsgBoxResult.Yes Then
+                Me.Close()
+            End If
+        Else MsgBox("Not valid", MsgBoxStyle.Critical)
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
